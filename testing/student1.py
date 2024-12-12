ef81fc2bf54d335425737187b164757c0ea544ab
montanus-wecib <amontanus@wcpss.net>
2024-12-11 19:27:00 -0500

    sync locations

---
+++ testing/student1.py
@@ -0,0 +1,5 @@
+# Students' function should look like this:
+def sphere_volume(radius=1):
+    """Calculate the volume of a sphere with a given radius."""
+    import math
+    return (4 / 3) * math.pi * radius**3
