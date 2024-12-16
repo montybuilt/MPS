d09e12967e38de07cd3164f3b69dd123634f486e
montanus-wecib <amontanus@wcpss.net>
2024-12-16 09:37:22 -0500

    remove testing from repo, keeping local

--- testing/student1.py
+++
@@ -1,5 +0,0 @@
-# Students' function should look like this:
-def sphere_volume(radius=1):
-    """Calculate the volume of a sphere with a given radius."""
-    import math
-    return (4 / 3) * math.pi * radius**3
