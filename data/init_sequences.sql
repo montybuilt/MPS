a507985902d6e8460ec2a2108a910972aa38d793
montanus-wecib <amontanus@wcpss.net>
2025-03-16 15:12:35 -0400

    simplified init sql

--- data/init_sequences.sql
+++ data/init_sequences.sql
@@ -1,14 +1,2 @@
--- Reset sequence for the user table
-SELECT setval('user_id_seq', (SELECT MAX(id) FROM "user") + 1);
-
--- Reset sequence for the questions table
-SELECT setval('questions_id_seq', (SELECT MAX(id) FROM questions) + 1);
-
--- Reset sequence for the curriculum_question table
+-- Reset sequence
 SELECT setval('curriculum_question_id_seq', (SELECT MAX(id) FROM curriculum_question) + 1);
-
--- Reset sequence for the content_curriculum table
-SELECT setval('content_curriculum_id_seq', (SELECT MAX(id) FROM content_curriculum) + 1);
-
--- Add other tables as needed (only those with data).
-
